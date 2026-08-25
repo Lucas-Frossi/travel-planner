cf798c4aba396dcd885ddc6b50888e6d861dcff1
Lucas-Frossi <lucas.teodoro@sptech.school>
2026-08-25 13:36:06 -0300

    chore: configure project with MySQL Docker

---
+++ backend/travel-planner/src/test/java/school/sptech/travelplanner/TravelPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.travelplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
